357b8bafb8e2cc192032fb2290e521d91001d469
silemi5 <silemi5@outlook.com>
2018-06-28 09:44:46 +0800

    Added Activity 2.

---
+++ Activity2/Assets/Scripts/MouseController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+
+	public float jetpackForce = 75.0f;
+	private Rigidbody2D playerRigidBody;
+
+	// Use this for initialization
+	void Start () 
+	{
+		playerRigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate()
+	{
+		bool jetpackActive = Input.GetButton("Fire1");
+
+		if(jetpackActive)
+		{
+			playerRigidBody.AddForce(new Vector2(0, jetpackForce));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
